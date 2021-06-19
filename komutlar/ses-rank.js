8a1e654c4e443224034d3197fad363503cd52940
Glitch (spangle-picturesque-lawyer) <none>
2021-06-19 21:33:50 +0000

    ☎️🎙 Checkpoint ./komutlar/ses-rank.js:31423971/478

--- komutlar/ses-rank.js
+++ komutlar/ses-rank.js
@@ -5,7 +5,12 @@ const target = message.mentions.users.first() || message.author; // Grab the tar
 const user = await client.discordVoice.fetch(target.id, message.guild.id); // Selects the target from the database.
 
 if (!user) return message.channel.send("Seems like this user does not have any Voice Activity so far...")
-  message.channel.send(`> **${target.tag}** currently has ${user.data.voiceTime.total}ms of Total Voice Time!`); // We show the voice time. (OPTIONAL: You can also use the ms package here if you want it to be more concise.)
+  
+  if (user.data.voiceTime.total === undefined) return message.channel.send("Seems like this user does not have any Voice Activity so far...");
+  
+  const dk = user.data.voiceTime.total / 60000
+  
+  message.channel.send(`> **${target.tag}** Şu Anda Toplam  ${dk} Dakika Ses Süresine sahip!`); // We show the voice time. (OPTIONAL: You can also use the ms package here if you want it to be more concise.)
 
   
 }
