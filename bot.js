c81a27f8b5606be801ebdd1afb6ab27963fb8bd8
Glitch (spangle-picturesque-lawyer) <none>
2021-06-20 10:57:57 +0000

    📟🏇 Checkpoint ./bot.js:31423971/1154 ./tepki.js:31423971/1154

--- bot.js
+++ bot.js
@@ -1598,53 +1598,7 @@ const randomAmountOfXp = Math.floor(Math.random() * 20) + 1; // Min 1, Max 30
 // setInterval(() => { let guild = client.guilds.cache.get("842418432905183242") ; let role = guild.roles.cache.get("842418432980549640") ; role.setColor("RANDOM") }, 65000);
 //rainbow
 
-//tepki rol
-let reactions = require("./reactions");
 
-client.on("ready", function() {
-  reactions.forEach(e => {
-    client.guilds.cache
-      .get(e.sunucu)
-      .channels.cache.get(e.kanal)
-      .messages.fetch(e.mesaj)
-      .then(mesaj => {
-        mesaj.react(e.emojiid);
-      });
-  });
-});
-
-client.on("messageReactionAdd", (messageReaction, user) => {
-  if (!user.bot) {
-    let member = messageReaction.message.guild.member(user);
-    reactions.forEach(async rs => {
-      if (rs.mesaj === messageReaction.message.id) {
-        if (messageReaction.emoji.id === rs.emojiid) {
-          if (!member.roles.cache.has(rs.rol)) {
-            member.roles.add(rs.rol);
-          }
-        }
-      }
-    });
-  }
-});
-
-client.on("messageReactionRemove", (messageReaction, user) => {
-  if (!user.bot) {
-    let member = messageReaction.message.guild.member(user);
-    reactions.forEach(async rs => {
-      if (rs.mesaj === messageReaction.message.id) {
-        if (messageReaction.emoji.id === rs.emojiid) {
-          if (member.roles.cache.has(rs.rol)) {
-            member.roles.remove(rs.rol);
-          }
-        }
-      }
-    });
-  }
-});
-
-
-//tepki rol
 
 
 //ses 
