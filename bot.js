6fe7e9058beed63110c84b5ed2e55c68aea9d584
Glitch (spangle-picturesque-lawyer) <none>
2021-06-25 11:22:13 +0000

    🍚🌥 Checkpoint ./bot.js:31423971/3609

--- bot.js
+++ bot.js
@@ -1791,3 +1791,161 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
 })
 
 //sesten çıkma
+
+
+
+
+
+
+
+//reaction rol 3 tane 
+
+
+
+//tepki rol
+let reactions = require("./reactions");
+
+client.on("ready", function() {
+  reactions.forEach(e => {
+    client.guilds.cache
+      .get(e.sunucu)
+      .channels.cache.get(e.kanal)
+      .messages.fetch(e.mesaj)
+      .then(mesaj => {
+        mesaj.react(e.emojiid);
+      });
+  });
+});
+
+client.on("messageReactionAdd", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (!member.roles.cache.has(rs.rol)) {
+            member.roles.add(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+client.on("messageReactionRemove", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (member.roles.cache.has(rs.rol)) {
+            member.roles.remove(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+
+//tepki rol
+
+
+//tepki rol
+
+client.on("ready", function() {
+  reactions.forEach(e => {
+    client.guilds.cache
+      .get(e.sunucu)
+      .channels.cache.get(e.kanal)
+      .messages.fetch(e.mesaj)
+      .then(mesaj => {
+        mesaj.react(e.emojiid);
+      });
+  });
+});
+
+client.on("messageReactionAdd", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (!member.roles.cache.has(rs.rol)) {
+            member.roles.add(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+client.on("messageReactionRemove", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (member.roles.cache.has(rs.rol)) {
+            member.roles.remove(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+
+//tepki rol
+
+
+
+//tepki rol
+
+client.on("ready", function() {
+  reactions.forEach(e => {
+    client.guilds.cache
+      .get(e.sunucu)
+      .channels.cache.get(e.kanal)
+      .messages.fetch(e.mesaj)
+      .then(mesaj => {
+        mesaj.react(e.emojiid);
+      });
+  });
+});
+
+client.on("messageReactionAdd", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (!member.roles.cache.has(rs.rol)) {
+            member.roles.add(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+client.on("messageReactionRemove", (messageReaction, user) => {
+  if (!user.bot) {
+    let member = messageReaction.message.guild.member(user);
+    reactions.forEach(async rs => {
+      if (rs.mesaj === messageReaction.message.id) {
+        if (messageReaction.emoji.id === rs.emojiid) {
+          if (member.roles.cache.has(rs.rol)) {
+            member.roles.remove(rs.rol);
+          }
+        }
+      }
+    });
+  }
+});
+
+
+//tepki rol
+
+
+//reaction rol 3 tane 
