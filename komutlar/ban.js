67d0fb1f0db53b2ad1195458269127c3b16c7bea
Glitch (spangle-picturesque-lawyer) <none>
2021-06-27 11:16:40 +0000

    👠🦂 Checkpoint ./komutlar/ban.js:31423971/2149

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -27,20 +27,27 @@ const { MessageButton } = require("discord-buttons");
  
 
    client.on('clickButton', async (button) => {
-      if (button.id === 'evet') {
-       await button.reply.send('My message'); //normal message, you can use also embed, buttons etc
+      
+     
+     
+     if (button.id === 'evet') {
 
-await button.reply.send('My messsage', true); //ephemeral message
-
-await button.reply.send('My message', { embed: embed, ephemeral: true }); //send embed with ephemeral message
-
-await button.reply.edit('My new reply'); //you can use the options which are above
-await button.reply.fetch(); 
+await button.reply.send('Banlandı ', true); //ephemeral message
+         
+        message.guild.members.cache.get(user.id).ban({
+  	reason: `${sebep}`
+          })
+      let embed = new Discord.MessageEmbed()
+    .setColor('0x36393E')
+    .setTitle('Kişi banlandı')
+    .addField('Yetkili', `${message.author.tag}`)
+    .addField('Banlanan kişi', user)
+    .addField('Sebep', sebep)
+    client.channels.cache.get(banlog).send(embed)
 
       }
         if (button.id === 'hayır') {
-        button.channel.send(new Discord.MessageEmbed().setFooter( "UMEF-EK / Discord'da Yeni Devrim!", client.user.avatarURL()) .setDescription(`[Oy Linkim](https://www.umefek.tk/oyver)`).setColor("BLUE").setAuthor("Oy Linki"));
-      } })
+await button.reply.send('Banlanma işlemi iptal edildi ', true);       } })
   
 	let rol = '842418432926679070';
   const embed = new Discord.MessageEmbed().setDescription('Ban yetkili Rolüne sahip değilsin.')
@@ -56,34 +63,7 @@ await button.reply.fetch();
     if (!message.guild.member(user).bannable) return message.reply(' ``Bu kişinin rolü senden üstte veya `Üyeleri yasakla` yetkisine sahip.``');
 
    message.channel.send('<@'+ user.id + '> Kişisini **'+ sebep+ '** Sebebiyle banlamak istediğine eminmisin ?' , { component: row }).then(async m => {
-   	 m.react('✅').then(r =>{ 
-
-   const tamam = (reaction,user) => reaction.emoji.name == '✅' && user.id == message.author.id;
-      const tamam2 = m.createReactionCollector(tamam)
-
-   tamam2.on('collect', async (r)=>{
-  message.guild.members.cache.get(user.id).ban({
-  	reason: `${sebep}`
-          })
-      let embed = new Discord.MessageEmbed()
-    .setColor('0x36393E')
-    .setTitle('Kişi banlandı')
-    .addField('Yetkili', `${message.author.tag}`)
-    .addField('Banlanan kişi', user)
-    .addField('Sebep', sebep)
-    client.channels.cache.get(banlog).send(embed)
-       })
-    })
-    await m.react('❌').then(r =>{ 
-
-   const tamam = (reaction,user) => reaction.emoji.name == '❌' && user.id == message.author.id;
-      const tamam2 = m.createReactionCollector(tamam)
-
-   tamam2.on('collect', async (r)=>{
-     m.delete()
-message.channel.send('Banlama işlemi iptal edildi.')
-      })
-    })
+   	
  })
 } 
 exports.conf = {
