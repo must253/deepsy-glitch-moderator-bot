f71a4fb2a88cfd9e246833bae64bd676370b96b1
Glitch (spangle-picturesque-lawyer) <none>
2021-07-09 09:37:23 +0000

    🕰🎪 Checkpoint ./komutlar/eğlence-arrow.js:31423971/33

--- komutlar/eğlence-arrow.js
+++ komutlar/eğlence-arrow.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   const yazi = args.slice(0).join('+'); 
 
-  if(!yazi) return message.channel.send(`**Lütfen yazı yazınız.** <a:maple_leaf:742698148329291826>`)
+  if(!yazi) return message.channel.send(`**Lütfen yazı yazınız.** `)
   const linqo = `https://dynamic.brandcrowd.com/asset/logo/1a2ebc7a-1b24-466a-bee7-9a0e8f5d8395/logo?v=4&text=${yazi}`
   .replace(' ', '+')
 
