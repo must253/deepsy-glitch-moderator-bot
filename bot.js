2f6e94f7ec1e8ef236e3f5029b4626200d4da505
Glitch (spangle-picturesque-lawyer) <none>
2021-07-10 14:48:31 +0000

    🏜🏙 Checkpoint ./bot.js:31423971/976

--- bot.js
+++ bot.js
@@ -1913,3 +1913,31 @@ musicprime.login(process.env.musicprime)
 
    
 
+client.on('ready', () => {
+    client.api.applications(client.user.id).guilds('842418432905183242 ').commands.post({
+        data: {
+            name: "hello",
+            description: "hello world command"
+            // possible options here e.g. options: [{...}]
+        }
+    });
+
+
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        const command = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+
+        if (command === 'hello'){ 
+            // here you could do anything. in this sample
+            // i reply with an api interaction
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: "hello world!!!"
+                    }
+                }
+            })
+        }
+    });
+});
