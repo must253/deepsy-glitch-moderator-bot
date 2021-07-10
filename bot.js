276d59e6719927676ebec3002216f4f087d3e4cc
Glitch (spangle-picturesque-lawyer) <none>
2021-07-10 15:10:41 +0000

    🐕👜 Checkpoint ./bot.js:31423971/7842 ./commands/say.js:31423971/574

--- bot.js
+++ bot.js
@@ -1911,53 +1911,5 @@ musicprime.login(process.env.musicprime)
    const Webhook = new Discord.WebhookClient(process.env.webhookid , process.env.webhooktoken)
 
 
-   const Bot = client
-const Commands = [];
-
    
-   
-Bot.on("ready", async () => {
-	for (const fileName of cmdFiles) {
-		const File = require(`./commands/${fileName}`);
-		Commands.push(File);
-		await Bot.api.applications(Bot.user.id).commands.post({
-			data: {
-				name: File.name,
-				description: File.description,
-				options: File.options,
-			},
-		});
-	}
-	console.info(`Logged in as ${Bot.user.username}`);
-});
 
-Bot.ws.on("INTERACTION_CREATE", (interaction) => {
-	const CMDFile = Commands.find(
-		(cmd) => cmd.name.toLowerCase() === interaction.data.name.toLowerCase(),
-	);
-	if (CMDFile)
-		CMDFile.execute(Bot, say, interaction, interaction.data.options);
-});
-
-Bot.login(Config.token);
-
-async function say(interaction, content) {
-	return Bot.api
-		.interactions(interaction.id, interaction.token)
-		.callback.post({
-			data: {
-				type: 4,
-				data: await createAPIMessage(interaction, content),
-			},
-		});
-}
-
-async function createAPIMessage(interaction, content) {
-	const apiMessage = await APIMessage.create(
-		Bot.channels.resolve(interaction.channel_id),
-		content,
-	)
-		.resolveData()
-		.resolveFiles();
-	return { ...apiMessage.data, files: apiMessage.files };
-}
